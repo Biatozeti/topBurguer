a52c879e8652aaffcd3336ee8fac51ac04a70ac8
Your Name <you@example.com>
2024-04-18 15:50:52 -0300

    carrinho

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -1,10 +1,39 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\CarrinhoFormRequest;
+use App\Http\Requests\CarrinhoRequest;
 use Illuminate\Http\Request;
 
 class CarrinhoController extends Controller
 {
-    //
+    public function index()
+    {
+        $carrinho = Carrinho::all();
+        return[
+            'cliente_id' => $carrinho->cliente_id,
+            'produto_id'=>$carrinho->produto_id,
+            'status'=> $carrinho->status,
+            'total'=>$carrinho->total
+        ]; 
+    }
+    public function store(CarrinhoRequest $request){
+        $carrinho = Carrinho::where('clientes_id', $request->clientes_id);
+        if(!$carrinho){
+            return response()->json([
+                "status"=>false,
+                "message"=> "cliente não encontrado"
+            ],400);
+        }
+        $carrinho = Carrinho::create([
+            'status'=>request->status,
+            'cliente_id'=>$request->cliente_id,
+            'total'=>$request->total,
+        ]);
+        return response()->json([
+            "status"=>true,
+            "message"=>"pedido feito com sucesso",
+            "data"=>$carrinho
+        ],200);
+    }
 }
--- app/Http/Requests/CarrinhoRequest.php
+++ app/Http/Requests/CarrinhoRequest.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Requests;
-
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CarrinhoRequest extends FormRequest
@@ -24,8 +25,29 @@ class CarrinhoRequest extends FormRequest
         return [
             'clientes_id'=>'required',
             'produtos_id'=>'required',
-            'status'=>'required',
-            'total'=>'required'
+            'status'=>'required|max:100|min:50',
+            'total'=>'required\decimal:2'
         ];
     }
+    public function  failedValidation(Validator $validator)
+
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'error' => $validator->errors()
+        ]));
+    }
+public function messages()
+{
+    return [
+        'clientes_id.required'=> 'o campo é obrigatório',
+        'produtos_id.required'=> 'o campo é obrigatorio',
+        'status.required'=> 'o campo é obrigatório',
+        'status.max'=>'o status deve conter no maximo 100 caracteres',
+        'status.min'=>'o status deve conter no minimo 50',
+        'total.required'=>'o campo é obrigatório',
+        'total.decimal'=> 'o campo total só aceita decimais'
+         ];
 }
+}
+  
